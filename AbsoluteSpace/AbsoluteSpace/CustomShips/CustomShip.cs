97b0cd91cb95cb99653030ad7eefd6b6854ca088
Shane Brouwer <shane@findesign.com.au>
2014-12-04 18:02:22 +1100

    add some spaceships

---
+++ AbsoluteSpace/AbsoluteSpace/CustomShips/CustomShip.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using org.flixel;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+
+namespace AbsoluteSpace.CustomShips
+{
+    class CustomShip : FlxGroup
+    {
+
+        public CustomShip()
+        {
+
+        }
+
+        override public void update()
+        {
+
+            base.update();
+
+        }
+
+
+    }
+}
---
+++ AbsoluteSpace/AbsoluteSpace/Globals.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using org.flixel;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Audio;
+
+namespace AbsoluteSpace
+{
+    public class Globals
+    {
+        public static int totalDistance = 0;
+
+        /// <summary>
+        /// Action global that includes the mouse.
+        /// </summary>
+        public static bool ACTION
+        {
+            get
+            {
+                return FlxG.keys.N ||
+                    FlxG.keys.X ||
+                    FlxG.keys.ENTER ||
+                    FlxG.keys.SPACE ||
+                    FlxG.gamepads.isButtonDown(Buttons.A) ||
+                    FlxG.gamepads.isButtonDown(Buttons.Start) || 
+                    FlxG.mouse.pressed();
+            }
+        }
+
+        public static bool ACTIONJUSTPRESSED
+        {
+            get
+            {
+                return  
+                    FlxG.keys.justPressed(Keys.N) ||
+                    FlxG.keys.justPressed(Keys.X) ||
+                    FlxG.keys.justPressed(Keys.Enter) ||
+                    FlxG.keys.justPressed(Keys.Space) ||
+                    FlxG.gamepads.isNewButtonPress(Buttons.A) ||
+                    FlxG.gamepads.isNewButtonPress(Buttons.Start) ||
+                    (FlxG.mouse.justPressed() );
+            }
+        }
+
+    }
+}
